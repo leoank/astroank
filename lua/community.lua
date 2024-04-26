cc5e103b88554e3d2d6e158ea9b6ae2ed3ed2b81
Ankur Kumar <ank@leoank.me>
2024-04-26 10:41:11 -0400

    Feat(plugins): Update neorg

--- lua/community.lua
+++ lua/community.lua
@@ -13,10 +13,10 @@ return {
     opts = {
       languages = {
         python = {
-          template = { annotation_convention = "numpydoc" }
-        }
-      }
-    }
+          template = { annotation_convention = "numpydoc" },
+        },
+      },
+    },
   },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.html-css" },
@@ -33,28 +33,15 @@ return {
   { import = "astrocommunity.scrolling.cinnamon-nvim" },
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
   { import = "astrocommunity.markdown-and-latex.peek-nvim" },
-  { import = "astrocommunity.markdown-and-latex.vimtex"},
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
-  { import = "astrocommunity.note-taking.neorg"},
+  { import = "astrocommunity.note-taking.neorg" },
   {
     "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
     opts = {
       load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.keybinds"] = {}, -- Adds default keybindings
-        ["core.export"] = {},
-        ["core.export.markdown"] = {},
-        ["core.completion"] = {
-          config = {
-            engine = "nvim-cmp",
-          },
-        }, -- Enables support for completion plugins
-        ["core.journal"] = {}, -- Enables support for the journal module
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
@@ -66,12 +53,12 @@ return {
       },
     },
   },
-  { import = "astrocommunity.note-taking.obsidian-nvim"},
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
   {
     "epwalsh/obsidian.nvim",
-    event = {"BufReadPre " .. vim.fn.expand "~" .. "/Sync/Documents/sangrah/*.md"},
+    event = { "BufReadPre " .. vim.fn.expand "~" .. "/Sync/Documents/sangrah/*.md" },
     opts = {
-      dir = vim.env.HOME .. "/Sync/Documents/sangrah"
-    }
+      dir = vim.env.HOME .. "/Sync/Documents/sangrah",
+    },
   },
 }
