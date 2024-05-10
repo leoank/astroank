3897255c8f1c9f2ef6ac7cb79078692f15a1ca63
Ankur Kumar <ank@leoank.me>
2024-05-10 15:34:40 -0400

    Feat(plugin): Add nvim-highlight-colors

--- plugins/community.lua
+++ plugins/community.lua
@@ -32,6 +32,7 @@ return {
   { import = "astrocommunity.markdown-and-latex.vimtex"},
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
+  { import = "astrocommunity.color.nvim-highlight-colors" },
   { import = "astrocommunity.note-taking.neorg"},
   {
     "nvim-neorg/neorg",
