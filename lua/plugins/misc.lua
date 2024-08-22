5de68cb20843e8cb9ada5049c72d7a4ba69278ee
Ankur Kumar <ank@leoank.me>
2024-08-22 19:32:21 -0400

    Feat(plugin: gpnvim): Add gpnvim agent

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -18,6 +18,24 @@ return {
             disable = false,
           },
         },
+
+        agents = {
+          {
+            provider = "ollama",
+            name = "DeepSeek Coder V2",
+            chat = true,
+            command = false,
+            -- string with model name or table with model name and parameters
+            model = {
+              model = "deepseek-coder-v2:latest",
+              temperature = 0.6,
+              top_p = 1,
+              min_p = 0.05,
+            },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are a coding AI assistant.",
+          },
+        },
       }
       require("gp").setup(conf)
     end,
