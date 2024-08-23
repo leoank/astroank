12fc5e6119bf0fcbfe17b710fc2dad8461d3e9e6
Ankur Kumar <ank@leoank.me>
2024-08-23 19:42:59 -0400

    Feat(plugin): Add undotree and trouble

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   -- { import = "astrocommunity.editing-support.yanky-nvim" },
+  { import = "astrocommunity.editing-support.undotree" },
   { import = "astrocommunity.editing-support.neogen" },
   {
     "danymat/neogen",
@@ -37,6 +38,7 @@ return {
   { import = "astrocommunity.markdown-and-latex.peek-nvim" },
   { import = "astrocommunity.markdown-and-latex.vimtex" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
   { import = "astrocommunity.color.nvim-highlight-colors" },
   { import = "astrocommunity.media.image-nvim" },
