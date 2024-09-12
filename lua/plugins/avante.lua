06ef9d8e2f32c879cfca7fc4193af54a8462ced6
Ankur Kumar <ank@leoank.me>
2024-09-12 12:06:31 -0400

    Feat(avante): Add more model providers

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -114,6 +114,52 @@ return {
           require("avante.providers").openai.parse_response(data_stream, event_state, opts)
         end,
       },
+      deepseeki = {
+        ["local"] = true,
+        endpoint = "karkinos:11434/v1",
+        model = "deepseek-coder-v2:16b-lite-instruct-fp16",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+      deepseekq = {
+        ["local"] = true,
+        endpoint = "karkinos:11434/v1",
+        model = "deepseek-coder-v2:236b-instruct-q2_K",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
     },
   },
   specs = { -- configure optional plugins
