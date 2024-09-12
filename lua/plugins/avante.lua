6246c26d2c4ab7ff401ee9f04b53feee050d1a6b
Ankur Kumar <ank@leoank.me>
2024-09-11 21:09:21 -0400

    Feat(avante): Add llama provider spec

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -71,7 +71,29 @@ return {
       ollama = {
         ["local"] = true,
         endpoint = "karkinos:11434/v1",
-        -- model = "codegemma",
+        model = "llama3.1:70b",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+      deepseek = {
+        ["local"] = true,
+        endpoint = "karkinos:11434/v1",
         model = "deepseek-coder-v2:latest",
         parse_curl_args = function(opts, code_opts)
           return {
