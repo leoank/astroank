61aeb89ba711ce965ba0f7756b724794c0c2d8d0
Ankur Kumar <ank@leoank.me>
2024-12-12 20:33:12 -0500

    chore(plugin: avante): udpate ollama spec

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -71,7 +71,7 @@ return {
       ollama = {
         ["local"] = true,
         endpoint = "karkinos:11434/v1",
-        model = "llama3.3",
+        model = "llama3.3:latest",
         parse_curl_args = function(opts, code_opts)
           return {
             url = opts.endpoint .. "/chat/completions",
